0c22489632364b78380b605771713cb4be08874c
nikelborm <evadev@duck.com>
2025-10-12 18:33:01 +0300

    quick save: Sun Oct 12 18:33:01 MSK 2025

--- packages/effect-schema-i18n/index.ts
+++ packages/effect-schema-i18n/index.ts
@@ -1,6 +1,8 @@
 import * as Effect from 'effect/Effect'
+import * as Either from 'effect/Either'
 import * as JSONSchema from 'effect/JSONSchema'
 import * as Schema from 'effect/Schema'
+import * as SchemaAST from 'effect/SchemaAST'
 
 const asd = Schema.Struct({
   asd_property1: Schema.String.annotations({
@@ -26,12 +28,12 @@ const asd = Schema.Struct({
     message: () => 'asd_property2 english message',
     parseIssueTitle: () => 'asd_property2 english parseIssueTitle',
   }),
-  'asd/asd': Schema.Date,
+  // 'asd/asd': Schema.Date,
 }).annotations({
   identifier: '@identifier/asd_struct',
   examples: [
-    { asd_property1: 'hello english', asd_property2: 1, 'asd/asd': new Date() },
-    { asd_property1: 'world english', asd_property2: 2, 'asd/asd': new Date() },
+    { asd_property1: 'hello english', asd_property2: 1 },
+    { asd_property1: 'world english', asd_property2: 2 },
   ],
   schemaId: 'asd_struct english schemaId',
   title: 'asd_struct english title',
@@ -41,16 +43,24 @@ const asd = Schema.Struct({
   parseIssueTitle: () => 'asd_struct english parseIssueTitle',
 })
 
-const asd2 = Schema.transform(asd, asd, {
+const asd2 = Schema.transformOrFail(asd, asd, {
   strict: true,
-  decode(fromA, fromI) {
-    return fromI
+  decode(fromA, options, ast, fromI) {
+    console.log('asd2 decode stuff validated')
+    return Either.right(fromI)
   },
-  encode(toI, toA) {
-    return toA
+  encode(toI, options, ast, toA) {
+    console.log('asd2 encode stuff validated')
+    return Either.right(toA)
   },
+  // decode(fromA, fromI) {
+  //
+  // },
+  // encode(toI, toA) {
+  //
+  // },
 })
 
-// console.log(JSON.stringify(JSONSchema.make(asd), null, 2));
-// console.log(JSONSchema.make(asd2));
-console.log(asd.annotations.toString())
+// console.log(JSON.stringify(JSONSchema.make(asd2), null, 2))
+console.log(JSONSchema.make(asd2))
+// console.log(asd2.ast)
