298e7f5ade31f8783bbc903774dd44a1cdb342fa
nikelborm <evadev@duck.com>
2026-01-04 03:04:22 +0300

    quick save: Sun Jan  4 03:04:22 MSK 2026

--- packages/next-midi-demo/src/app/lib/MIDIDeviceSelect.tsx
+++ packages/next-midi-demo/src/app/lib/MIDIDeviceSelect.tsx
@@ -28,35 +28,34 @@ import {
   SelectValue,
 } from './Select.tsx'
 
-const portMapAtom = Atom.make(
-  Effect.fn(function* (ctx) {
-    const access = yield* EMIDIAccess.EMIDIAccess
-    const portsMapRef = yield* Ref.make(yield* EMIDIAccess.AllPortsRecord)
+type UpdatePortMapFn = (
+  portMap: EMIDIPort.IdToInstanceMap,
+) => EMIDIPort.IdToInstanceMap
 
-    yield* EFunction.pipe(
-      EMIDIAccess.makeAllPortsStateChangesStream(access),
-      Stream.tap(({ port, newState }) =>
-        Effect.gen(function* () {
-          const newVal = yield* Ref.updateAndGet(
-            portsMapRef,
-            (newState.ofDevice === 'disconnected'
-              ? Record.remove(port.id)
-              : Record.set(port.id, port)) as (
-              portMap: EMIDIPort.IdToInstanceMap,
-            ) => EMIDIPort.IdToInstanceMap,
-          )
-          yield* Effect.log('updated ref')
+const portMapAtom = Effect.gen(function* () {
+  const initialValue = yield* EMIDIAccess.AllPortsRecord
+  const portsMapRef = yield* Ref.make(initialValue)
 
-          yield* Effect.sync(() => ctx.setSelf(Result.success(newVal)))
-        }),
-      ),
-      Stream.runDrain,
-      Effect.forkScoped,
-    )
-    return yield* Ref.get(portsMapRef)
-  }, Effect.provide(EMIDIAccess.layerMostRestricted)),
-  { initialValue: {} },
-).pipe(Atom.debounce(Duration.millis(1)), Atom.withServerValueInitial)
+  return Stream.concat(
+    Stream.succeed(initialValue),
+    Stream.mapEffect(
+      EMIDIAccess.makeAllPortsStateChangesStreamInContext(),
+      ({ port, newState }) =>
+        Ref.updateAndGet(
+          portsMapRef,
+          (newState.ofDevice === 'disconnected'
+            ? Record.remove(port.id)
+            : Record.set(port.id, port)) as UpdatePortMapFn,
+        ),
+    ),
+  )
+}).pipe(
+  Stream.unwrap,
+  Stream.provideLayer(EMIDIAccess.layerMostRestricted),
+  portMapUpdatesStream => Atom.make(portMapUpdatesStream),
+  Atom.debounce(Duration.millis(20)),
+  Atom.withServerValueInitial,
+)
 
 const selectedId = Atom.make(null as EMIDIPort.BothId | null)
 
@@ -68,84 +67,83 @@ export const MIDIDeviceSelect = ({
   typeToShowExclusively?: 'input' | 'output' | undefined
 }) => {
   const portMapResult = useAtomValue(portMapAtom)
+  console.log('MIDIDeviceSelect rendered: ', portMapResult)
 
   const setSelectedId = useAtomSet(selectedId)
 
-  return (
-    Result.matchWithWaiting(portMapResult, {
-      onDefect: defect => {
-        console.log('defect', defect)
-        throw defect
-      },
-      onError: (error, _) => <span>Error: {error._tag}</span>,
-      onWaiting: _ => (
-        <SelectRoot name="select_port" disabled>
+  return Result.matchWithError(portMapResult, {
+    onDefect: defect => {
+      console.log('defect', defect)
+      throw defect
+    },
+    onError: error => <span>Error: {error._tag}</span>,
+    onInitial: () => (
+      <SelectRoot name="select_port" disabled>
+        <SelectTrigger>
+          <MonoPre>Loading ports...</MonoPre>
+        </SelectTrigger>
+      </SelectRoot>
+    ),
+    onSuccess: ({ value: portMap }) => {
+      const filteredPorts = EArray.filter(
+        Record.values(portMap),
+        port => !typeToShowExclusively || port.type === typeToShowExclusively,
+      )
+      return (
+        <SelectRoot<EMIDIPort.BothId | null>
+          onValueChange={setSelectedId}
+          name="select_port"
+          items={EFunction.pipe(
+            filteredPorts,
+            EArray.map(port => ({
+              label: (
+                <PortLabel port={port} showType={!typeToShowExclusively} />
+              ),
+              value: port.id,
+            })),
+            EArray.append({
+              label: <MonoPre>Select port ID</MonoPre>,
+              value: null,
+            }),
+          )}
+        >
           <SelectTrigger>
-            <MonoPre>Loading ports...</MonoPre>
+            <SelectValue />
+            <SelectIcon>
+              <ChevronUpDownSVG />
+            </SelectIcon>
           </SelectTrigger>
+          <SelectPortal>
+            <SelectPositioner sideOffset={8}>
+              <SelectPopup>
+                <SelectScrollUpArrow />
+                <SelectList>
+                  {EFunction.pipe(
+                    EArray.map(filteredPorts, port => (
+                      <SelectItem key={port.id} value={port.id}>
+                        <SelectItemIndicator>
+                          <SelectItemIndicatorIcon />
+                        </SelectItemIndicator>
+                        <SelectItemText>
+                          <PortLabel
+                            port={port}
+                            // only show the type annotation when the type of
+                            // elements is not obvious
+                            showType={!typeToShowExclusively}
+                          />
+                        </SelectItemText>
+                      </SelectItem>
+                    )),
+                  )}
+                </SelectList>
+                <SelectScrollDownArrow />
+              </SelectPopup>
+            </SelectPositioner>
+          </SelectPortal>
         </SelectRoot>
-      ),
-      onSuccess: ({ value: portMap }) => {
-        const filteredPorts = EArray.filter(
-          Record.values(portMap),
-          port => !typeToShowExclusively || port.type === typeToShowExclusively,
-        )
-        return (
-          <SelectRoot<EMIDIPort.BothId | null>
-            onValueChange={setSelectedId}
-            name="select_port"
-            items={EFunction.pipe(
-              filteredPorts,
-              EArray.map(port => ({
-                label: (
-                  <PortLabel port={port} showType={!typeToShowExclusively} />
-                ),
-                value: port.id,
-              })),
-              EArray.append({
-                label: <MonoPre>Select port ID</MonoPre>,
-                value: null,
-              }),
-            )}
-          >
-            <SelectTrigger>
-              <SelectValue />
-              <SelectIcon>
-                <ChevronUpDownSVG />
-              </SelectIcon>
-            </SelectTrigger>
-            <SelectPortal>
-              <SelectPositioner sideOffset={8}>
-                <SelectPopup>
-                  <SelectScrollUpArrow />
-                  <SelectList>
-                    {EFunction.pipe(
-                      EArray.map(filteredPorts, port => (
-                        <SelectItem key={port.id} value={port.id}>
-                          <SelectItemIndicator>
-                            <SelectItemIndicatorIcon />
-                          </SelectItemIndicator>
-                          <SelectItemText>
-                            <PortLabel
-                              port={port}
-                              // only show the type annotation when the type of
-                              // elements is not obvious
-                              showType={!typeToShowExclusively}
-                            />
-                          </SelectItemText>
-                        </SelectItem>
-                      )),
-                    )}
-                  </SelectList>
-                  <SelectScrollDownArrow />
-                </SelectPopup>
-              </SelectPositioner>
-            </SelectPortal>
-          </SelectRoot>
-        )
-      },
-    }) ?? "isn't a result"
-  )
+      )
+    },
+  })
 }
 
 const PortLabel = ({
