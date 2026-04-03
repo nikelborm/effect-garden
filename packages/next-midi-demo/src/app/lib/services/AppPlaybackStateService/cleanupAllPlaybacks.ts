1587bbfaf4811b3ac59efd2e86cb907b6fe53dcf
nikelborm <evadev@duck.com>
2026-04-03 14:46:20 +0300

    quick save: Fri Apr  3 14:46:20 MSK 2026

--- packages/next-midi-demo/src/app/lib/services/AppPlaybackStateService/cleanupAllPlaybacks.ts
+++ packages/next-midi-demo/src/app/lib/services/AppPlaybackStateService/cleanupAllPlaybacks.ts
@@ -7,23 +7,25 @@ import { minLoudness, transitionTimeInSeconds } from './constants.ts'
 import { helpGarbageCollectionOfPlayback } from './playbackNodes/index.ts'
 import type { PlayingAppPlaybackStates } from './types/index.ts'
 
-export const makeCleanupAllPlaybacks = (audioContext: EAudioContext.Instance) =>
-  Effect.fn(function* (state: PlayingAppPlaybackStates) {
-    const secondsSinceAudioContextInit =
-      yield* EAudioContext.currentTime(audioContext)
+export const cleanupAllPlaybacks = Effect.fn(function* (
+  state: PlayingAppPlaybackStates,
+) {
+  const audioContext = yield* EAudioContext.EAudioContext
+  const secondsSinceAudioContextInit =
+    yield* EAudioContext.currentTime(audioContext)
 
-    yield* Effect.forEach(
-      state.transitionQueue.map(_ => _.playback),
-      playback => {
-        playback.gainNode.gain.exponentialRampToValueAtTime(
-          minLoudness,
-          secondsSinceAudioContextInit + transitionTimeInSeconds,
-        )
+  yield* Effect.forEach(
+    state.transitionQueue.map(_ => _.playback),
+    playback => {
+      playback.gainNode.gain.exponentialRampToValueAtTime(
+        minLoudness,
+        secondsSinceAudioContextInit + transitionTimeInSeconds,
+      )
 
-        return helpGarbageCollectionOfPlayback(playback).pipe(
-          Effect.delay(Duration.seconds(transitionTimeInSeconds + 0.1)),
-        )
-      },
-      { discard: true },
-    ).pipe(Effect.tapErrorCause(Effect.logError), Effect.forkDaemon)
-  })
+      return helpGarbageCollectionOfPlayback(playback).pipe(
+        Effect.delay(Duration.seconds(transitionTimeInSeconds + 0.1)),
+      )
+    },
+    { discard: true },
+  ).pipe(Effect.tapErrorCause(Effect.logError), Effect.forkDaemon)
+})
