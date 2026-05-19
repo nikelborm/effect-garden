225a0f44bc127de033e95045fc243d2b2d9aade8
nikelborm <evadev@duck.com>
2026-05-19 19:25:56 +0300

    quick save: Tue May 19 19:25:56 MSK 2026

---
+++ packages/vscode-json-schemas-offline/scripts/fetch-schemas-effect.ts
@@ -0,0 +1,6 @@
+// TODO: when targeting single-file schemas (automatically packaging about 200
+// vs code extensions or more) a cool strategy for rebuilding the extension
+// would be running it against git diff between commits. we can make an
+// extension build on commit. remember hash, and when the time passes, we can
+// pull the updates, see the relative diff since last release, and only
+// rerelease the neccessary packages
