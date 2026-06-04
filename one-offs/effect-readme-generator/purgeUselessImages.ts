b2b9cba793064b534a96d89cda489fc27e2dee1a
nikelborm <evadev@duck.com>
2026-06-04 12:01:49 +0300

    quick save: Thu Jun  4 12:01:49 MSK 2026

--- one-offs/effect-readme-generator/purgeUselessImages.ts
+++ one-offs/effect-readme-generator/purgeUselessImages.ts
@@ -1,44 +1,61 @@
 #!/usr/bin/env bun
 
-import { readdir, readFile, rm } from 'node:fs/promises'
+import * as FileSystem from '@effect/platform/FileSystem'
+import * as Path from '@effect/platform/Path'
+import * as BunContext from '@effect/platform-bun/BunContext'
+import * as BunRuntime from '@effect/platform-bun/BunRuntime'
+import * as Effect from 'effect/Effect'
 
 import { END_TOKEN, README_FILE_PATH, START_TOKEN } from './src/constants.ts'
 import { getImageFileName } from './src/getPathToImageInRepo.ts'
 import { extractReposFromMarkdownSoft } from './src/markdownPinToAndFrom.ts'
 import { TokenReplacer } from './src/splitStringApart.ts'
 
-const oldReadme = await readFile(README_FILE_PATH, 'utf8')
+const program = Effect.gen(function* () {
+  const fs = yield* FileSystem.FileSystem
+  const path = yield* Path.Path
 
-const replacer = new TokenReplacer(oldReadme, {
-  repos: [START_TOKEN, END_TOKEN],
+  const oldReadme = yield* fs.readFileString(README_FILE_PATH)
+
+  const replacer = new TokenReplacer(oldReadme, {
+    repos: [START_TOKEN, END_TOKEN],
+  })
+
+  const reposMarkdownTableString =
+    replacer.getPartsOnFirstMatchOrThrow('repos').targetPartExcludingTokens
+
+  const expectedToHaveImageFileNames = new Set(
+    extractReposFromMarkdownSoft(reposMarkdownTableString)
+      .filter(e => e.imageHost === 'raw.githubusercontent.com')
+      .map(e =>
+        getImageFileName({ name: e.repoName, owner: e.username }, e.themeName),
+      ),
+  )
+
+  yield* Effect.log(
+    'Expected to have image file names: ',
+    expectedToHaveImageFileNames,
+  )
+
+  const presentImageFileNames = new Set(
+    (yield* fs.readDirectory('./images')).filter(
+      e => !['.gitkeep', '.gitignore'].includes(e),
+    ),
+  )
+
+  yield* Effect.log('Present image file names: ', presentImageFileNames)
+
+  const removalTargets = presentImageFileNames.difference(
+    expectedToHaveImageFileNames,
+  )
+
+  yield* Effect.forEach(
+    removalTargets,
+    target => fs.remove(path.join('images', target), { recursive: true }),
+    { concurrency: 'unbounded' },
+  )
+
+  yield* Effect.log('Deleted following files in images folder:', removalTargets)
 })
 
-const reposMarkdownTableString =
-  replacer.getPartsOnFirstMatchOrThrow('repos').targetPartExcludingTokens
-
-const expectedToHaveImageFileNames = new Set(
-  extractReposFromMarkdownSoft(reposMarkdownTableString)
-    .filter(e => e.imageHost === 'raw.githubusercontent.com')
-    .map(e =>
-      getImageFileName({ name: e.repoName, owner: e.username }, e.themeName),
-    ),
-)
-
-console.log('Expected to have image file names: ', expectedToHaveImageFileNames)
-
-process.chdir('./images')
-
-const presentImageFileNames = new Set(
-  (await readdir('.')).filter(e => !['.gitkeep', '.gitignore'].includes(e)),
-)
-
-console.log('Present image file names: ', presentImageFileNames)
-
-const removalTargets = presentImageFileNames.difference(
-  expectedToHaveImageFileNames,
-)
-
-for (const target of removalTargets)
-  await rm(target, { force: true, recursive: true })
-
-console.log(`Deleted following files in images folder:`, removalTargets)
+program.pipe(Effect.provide(BunContext.layer), BunRuntime.runMain)
