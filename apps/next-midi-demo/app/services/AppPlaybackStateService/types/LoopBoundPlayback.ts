96e752d9b4566ae0829b5ce458e912b23ca17259
nikelborm <evadev@duck.com>
2026-06-29 15:45:33 +0300

    quick save: Mon Jun 29 15:45:33 MSK 2026

--- apps/next-midi-demo/app/services/AppPlaybackStateService/types/LoopBoundPlayback.ts
+++ apps/next-midi-demo/app/services/AppPlaybackStateService/types/LoopBoundPlayback.ts
@@ -73,7 +73,7 @@ export class LoopBoundPlayback extends Schema.TaggedClass<LoopBoundPlayback>()(
     transitionQueue: LoopBoundQueue,
   },
 ) {
-  private declare '~brand~': never
+  protected declare '~brand~': never
   static {
     this.make = this.make.bind(this)
   }
