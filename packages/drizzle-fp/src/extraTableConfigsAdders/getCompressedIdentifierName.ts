a1360f5143e353947a2788c78784027289ee7a72
nikelborm <evadev@duck.com>
2026-09-20 15:14:55 +0300

    quick save: Sun Sep 20 03:14:55 PM MSK 2026

--- packages/drizzle-fp/src/extraTableConfigsAdders/getCompressedIdentifierName.ts
+++ packages/drizzle-fp/src/extraTableConfigsAdders/getCompressedIdentifierName.ts
@@ -15,11 +15,8 @@ export function getCompressedIdentifierName(prefix: string, tokens: string[]) {
       break
     }
     const index = tokens.findLastIndex(e => e.length === maxLength)
-    if (index in tokens) {
-      if(!tokens[index]) throw new Error('Absurd')
-
-      tokens[index] = tokens[index].slice(0, 1)
-    }
+    const val = tokens[index];
+    if (val) tokens[index] = val.slice(0, 1)
     else throw new Error('wtf in getCompressedIdentifierName')
     rendered = render()
   }
